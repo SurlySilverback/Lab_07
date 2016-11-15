52ab8e872407a4f47d35cd87db0136aa822aa506
SurlySilverback <SurlySilverback@github.com>
2016-11-14 16:16:58 -0800

    All files ready to go

---
+++ Iterator.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "composite.h"
+
+class Iterator{
+
+    protected:
+        Base* self_ptr:
+        Base* current_ptr;
+
+    public:
+        Iterator ( Base* ptr ) { this->self_ptr = ptr; }
+
+        virtual void first() = 0;
+        virtual void next() = 0;
+        virtual bool is_done() = 0;
+        virtual Base* current() = 0;
+};
