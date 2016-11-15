9c18031b5c445b1ae0b70185710e3b8e98365128
SurlySilverback <SurlySilverback@github.com>
2016-11-14 18:56:59 -0800

    PreOrderIterator update

--- PreOrderIterator.cpp
+++ PreOrderIterator.cpp
@@ -1,25 +1,54 @@
 #include "PreOrderIterator.h"
 
-void PreOrderIterator::first(){
+void PreOrderIterator::first()
+{
+    // FIXME: Make sure this constructs a new pointer correctly
+    Iterator it = this->self_ptr->CreateIterator();
 
-    Iterator it = this->
+    if (it) // check if not NULL
+    {
+        it->First();
+        
+        for ( unsigned int i = 0; i < iterators.size(); ++i )
+        {
+            iterators.pop();
+        }
 
+        this->iterators.push(it);
+    }
 }
 
-void PreOrderIterator::next(){
+void PreOrderIterator::next()
+{
+    Iterator it = iterators.top()->current()->CreateIterator();
+    it->first();
+    iterators.push(it)
 
-
-
+    while ( iterators.size() > 0 && iterators.top()->is_done() )
+    {
+        delete iterators.pop();
+        
+        if (iterators.top != NULL )
+        {
+            iterators.top()->next();
+        }
+    }
 }
 
-void PreOrderIterator::is_done(){
+void PreOrderIterator::is_done()
+{
+    if ( iterators.size() == 0 )
+        return true;
 
-
-
+    else
+        return false;
 }
 
-Base* PreOrderIterator::current(){
-
-
-
+Base* PreOrderIterator::current()
+{
+    if ( this->iterators.Size() > 0 )
+        return this->iterators.Top()->current();
+    
+    else
+        return NULL;
 }
